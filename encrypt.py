248f6286e67cbcf89270c05eb387afa4c31c839f
drdraze <ben.drazen@gmail.com>
2024-08-16 13:05:30 +0100

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -5,6 +5,3 @@ class CaesarCipher:
         for char in string:
             encrypted += chr((ord(char) + d - ord("a")) % 26 + ord("a"))
         return encrypted
-
-    def _char_distance(self, char1, char2):
-        return (ord(char1) - ord(char2)) % 26
